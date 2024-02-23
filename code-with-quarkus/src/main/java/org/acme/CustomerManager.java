0b5ff9cea9afa300db7317e6327b655270cbbefa
misanki <ankitmishra471@gmail.com>
2024-02-23 15:46:05 +0530

    cleaned code customerManager

--- code-with-quarkus/src/main/java/org/acme/CustomerManager.java
+++ code-with-quarkus/src/main/java/org/acme/CustomerManager.java
@@ -19,24 +19,9 @@ public class CustomerManager {
     @POST
     @Path("/create")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response saveCustomer(Customer customer) {
+    public Response createCustomer(Customer customer) {
         System.out.println("got request " + customer.firstName);
         return customerService.updateCustomerDetails(customer);
     }
 
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Customer getCustomers(@PathParam("id") String id) {
-        Customer customer = customerService.getCustomer(id);
-        return customer;
-    }
-
-    @GET
-    @Path("/nameAndCity/{name}/{city}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Customer> getCustomers(@PathParam("firstName") String name, @PathParam("age") int age) {
-        List<Customer> customers = customerService.getByNameAndCity("Ankit", 30);
-        return customers;
-    }
 }
