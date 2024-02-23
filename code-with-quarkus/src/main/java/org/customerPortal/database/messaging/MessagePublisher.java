b90ba9ebdcbfe16292fa9f41f751e23972586300
misanki <ankitmishra471@gmail.com>
2024-02-23 21:01:46 +0530

    handled Kafka failure

--- code-with-quarkus/src/main/java/org/customerPortal/database/messaging/MessagePublisher.java
+++ code-with-quarkus/src/main/java/org/customerPortal/database/messaging/MessagePublisher.java
@@ -6,6 +6,7 @@ import org.customerPortal.database.Customer;
 import org.customerPortal.services.CustomerValidator;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +15,18 @@ public class MessagePublisher {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerValidator.class);
     @Inject
     @Channel("newCustomer")
-    Emitter<Record<String, Customer>> empEmitter;
+    Emitter<Customer> empEmitter;
 
     public void publishCustomer(Customer customer) {
         LOGGER.info("Publishing Customer message to topic : {}", customer.getCustomerId());
-        empEmitter.send(Record.of(String.valueOf(customer.getCustomerId()), customer));
+        empEmitter.send(customer)
+                .exceptionally(e -> handleFailure(customer, e));
         System.out.println("Recieveed messave");
+    }
+    private Void handleFailure(Customer customer, Throwable throwable) {
+        LOGGER.error("Failed to publish message to kafka for customer: {}", customer.getCustomerId());
+        LOGGER.error("Error details:", throwable);
+        return null; // or throw a specific exception if you want to propagate it further
     }
 
 }
